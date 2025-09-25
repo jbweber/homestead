2c4193ace3b5f27d43dfab4ace0c8832d999aaf5
Jeff Weber <jweber@cofront.net>
2025-09-24 21:27:03 -0400

    bridge networking + kubevirt

--- kubernetes/install-cilium.sh
+++ kubernetes/install-cilium.sh
@@ -1,5 +1,8 @@
-cilium install --version 1.18.1 \
+cilium install --version 1.18.2 \
   --set kubeProxyReplacement=true \
   --set bgpControlPlane.enabled=true \
   --set ipam.mode=kubernetes \
-  --set cni.binPath=/usr/libexec/cni
+  --set cni.binPath=/usr/libexec/cni \
+  --set cni.exclusive=false
+
+  kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml
